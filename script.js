c1f79c78605335e0982f0bcbf91ab35410def1b0
tommasodalessio <117944144+tommasodalessio@users.noreply.github.com>
2023-07-30 10:32:24 +0200

    creation of a function for a round of the game

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-console.log("Hello World");
 
-/* create a function called getComputerChoice that will randomly return: 
-    - Rock 
-    - Paper 
-    - Scissors
-*/
-
-//create a function named getComputerChoice
 function getComputerChoice () {
 //create a varible for the return value
 let answer;
 //use the method random, asking a random number from o to 2 
 let x = Math.floor(Math.random() * 3);
-console.log(x)
 //if the number is 0 -> Rock 
 if (x == 0) {
     answer = "Rock";
@@ -26,10 +17,48 @@ else {
     answer = "Scissors";
 }
 //return the value +
-console.log(answer)
 return answer;
 }
+function game (playerSelection, computerSelection) {
+    //made a varible that contains the answer
+    let answer_value;
+    // made the playerSelection and the computerSelection case insensitive  
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    //check the playerSelection if it's diffenent from rock paper or scissors
+    console.log(computerSelection);
+    if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors"){
+        //if they are the same -> drawn
+        if (playerSelection == computerSelection) {
+            answer_value = "It's a drawn, sad!!"
+        }
+        //if they are different: 
+        else {
+            //  - rock beats scissors 
+            if (playerSelection == "rock" && computerSelection == "scissors"){
+                answer_value = "You Win! Rock beats Scissors";
+            }
+            //  - scissors beats paper 
+            else if (playerSelection == "scissors" && computerSelection == "paper"){
+                answer_value = "You Win! Scissors beats Paper";
+            }
+            //  - paper beats rock 
+            else if (playerSelection == "paper" && computerSelection == "rock"){
+                answer_value = "You Win! Paper beats Rock";
+            }
+            else {
+                playerSelection = playerSelection[0].toUpperCase() + playerSelection.substring(1);
+                computerSelection = computerSelection[0].toUpperCase() + computerSelection.substring(1);
+                answer_value = "You Lost! " + computerSelection + " beats " + playerSelection;
+            }
 
-getComputerChoice();
+        }
+    
+    }
+    else {
+        answer_value = "Invalid player selection, try again";
+    }
+    return answer_value;
+}
 
 
